ed28e843954a50d1ca7d33dbcce4a6e58e638b91
skaball69@gmail.com <skaball69@gmail.com>
2022-04-02 21:07:00 +0700

    sdfsdf

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -97,6 +97,8 @@ class HomeController extends Controller
                 $promotion_costs = PromotionCost::whereCustomerId($customer->id)->wherePromotionId($promotion_query->id) 
                         ->whereBetween('created_at', [$dates[0], $dates[1]])->get();
 
+                dd($promotion_cost,$promotion_query->amount_per_day);
+
                 if(!($promotion_costs->count() != 0) && ($promotion_costs->count() >= $promotion_query->amount_per_day)) {
 
                     $promotion_query['active'] = 0;
